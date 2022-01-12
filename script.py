80ea94c794806df5ea2ebd78908a653e400be6f2
DalegendaryCat <50706524+DalegendaryCat@users.noreply.github.com>
2022-01-12 11:05:14 +0800

    Update script.py

--- script.py
+++ script.py
@@ -20,22 +20,22 @@ TF_RECORD_SCRIPT_NAME = 'generate_tfrecord.py'
 LABEL_MAP_NAME = 'label_map.pbtxt'
 
 paths = {
-    'WORKSPACE_PATH': os.path.join('/Users/nguyenvanbo/Desktop/WOW/image processing/Tensorflow', 'workspace'),
-    'SCRIPTS_PATH': os.path.join('/Users/nguyenvanbo/Desktop/WOW/image processing/Tensorflow','scripts'),
-    'APIMODEL_PATH': os.path.join('/Users/nguyenvanbo/Desktop/WOW/image processing/Tensorflow','models'),
-    'ANNOTATION_PATH': os.path.join('/Users/nguyenvanbo/Desktop/WOW/image processing/Tensorflow', 'workspace','annotations'),
-    'IMAGE_PATH': os.path.join('/Users/nguyenvanbo/Desktop/WOW/image processing/Tensorflow', 'workspace','images'),
-    'MODEL_PATH': os.path.join('/Users/nguyenvanbo/Desktop/WOW/image processing/Tensorflow', 'workspace','models'),
-    'PRETRAINED_MODEL_PATH': os.path.join('/Users/nguyenvanbo/Desktop/WOW/image processing/Tensorflow', 'workspace','pre-trained-models'),
-    'CHECKPOINT_PATH': os.path.join('/Users/nguyenvanbo/Desktop/WOW/image processing/Tensorflow', 'workspace','models',CUSTOM_MODEL_NAME), 
-    'OUTPUT_PATH': os.path.join('/Users/nguyenvanbo/Desktop/WOW/image processing/Tensorflow', 'workspace','models',CUSTOM_MODEL_NAME, 'export'), 
-    'TFJS_PATH':os.path.join('/Users/nguyenvanbo/Desktop/WOW/image processing/Tensorflow', 'workspace','models',CUSTOM_MODEL_NAME, 'tfjsexport'), 
-    'TFLITE_PATH':os.path.join('/Users/nguyenvanbo/Desktop/WOW/image processing/Tensorflow', 'workspace','models',CUSTOM_MODEL_NAME, 'tfliteexport'), 
-    'PROTOC_PATH':os.path.join('/Users/nguyenvanbo/Desktop/WOW/image processing/Tensorflow','protoc')
+    'WORKSPACE_PATH': os.path.join('./Tensorflow', 'workspace'), #make changes in the pathname where necessary
+    'SCRIPTS_PATH': os.path.join('./Tensorflow','scripts'),
+    'APIMODEL_PATH': os.path.join('./Tensorflow','models'),
+    'ANNOTATION_PATH': os.path.join('./Tensorflow', 'workspace','annotations'),
+    'IMAGE_PATH': os.path.join('./Tensorflow', 'workspace','images'),
+    'MODEL_PATH': os.path.join('./Tensorflow', 'workspace','models'),
+    'PRETRAINED_MODEL_PATH': os.path.join('./Tensorflow', 'workspace','pre-trained-models'),
+    'CHECKPOINT_PATH': os.path.join('./Tensorflow', 'workspace','models',CUSTOM_MODEL_NAME), 
+    'OUTPUT_PATH': os.path.join('./Tensorflow', 'workspace','models',CUSTOM_MODEL_NAME, 'export'), 
+    'TFJS_PATH':os.path.join('./Tensorflow', 'workspace','models',CUSTOM_MODEL_NAME, 'tfjsexport'), 
+    'TFLITE_PATH':os.path.join('./Tensorflow', 'workspace','models',CUSTOM_MODEL_NAME, 'tfliteexport'), 
+    'PROTOC_PATH':os.path.join('./Tensorflow','protoc')
  }
 
 files = {
-    'PIPELINE_CONFIG':os.path.join('/Users/nguyenvanbo/Desktop/WOW/image processing/Tensorflow', 'workspace','models', CUSTOM_MODEL_NAME, 'pipeline.config'),
+    'PIPELINE_CONFIG':os.path.join('./Tensorflow', 'workspace','models', CUSTOM_MODEL_NAME, 'pipeline.config'),
     'TF_RECORD_SCRIPT': os.path.join(paths['SCRIPTS_PATH'], TF_RECORD_SCRIPT_NAME), 
     'LABELMAP': os.path.join(paths['ANNOTATION_PATH'], LABEL_MAP_NAME)
 }
